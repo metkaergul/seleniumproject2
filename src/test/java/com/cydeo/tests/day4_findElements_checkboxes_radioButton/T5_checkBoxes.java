bc9401c4ba2ef5c7f7cdecb4f7f7e8ad9830d3f7
metkaergul <metkaergul@gmail.com>
2022-08-23 13:03:54 +0300

    day4 task5_checkboxes

---
+++ src/test/java/com/cydeo/tests/day4_findElements_checkboxes_radioButton/T5_checkBoxes.java
@@ -0,0 +1,28 @@
+package com.cydeo.tests.day4_findElements_checkboxes_radioButton;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class T5_checkBoxes {
+    public static void main(String[] args) {
+
+        //Practice: Checkboxes
+
+        WebDriver driver= WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        //1. Go to https://practice.cydeo.com/checkboxes
+        driver.get("https://practice.cydeo.com/checkboxes");
+
+        //2. Confirm checkbox #1 is NOT selected by default
+
+        //3. Confirm checkbox #2 is SELECTED by default.
+        //4. Click checkbox #1 to select it.
+        //5. Click checkbox #2 to deselect it.
+        //6. Confirm checkbox #1 is SELECTED.
+        //7. Confirm checkbox #2 is NOT selected.
+
+    }
+}
