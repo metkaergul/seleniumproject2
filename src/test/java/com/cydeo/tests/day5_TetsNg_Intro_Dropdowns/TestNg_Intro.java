383fdb5f2d3eabebf2dc8f4ae5281715bde84783
metkaergul <metkaergul@gmail.com>
2022-08-29 10:02:51 +0300

    DAY5 testNg Intro

---
+++ src/test/java/com/cydeo/tests/day5_TetsNg_Intro_Dropdowns/TestNg_Intro.java
@@ -0,0 +1,12 @@
+package com.cydeo.tests.day5_TetsNg_Intro_Dropdowns;
+
+import org.testng.annotations.Test;
+
+public class TestNg_Intro {
+
+
+    @Test
+    public void test1(){
+        System.out.println("test1 is runnung");
+    }
+}
