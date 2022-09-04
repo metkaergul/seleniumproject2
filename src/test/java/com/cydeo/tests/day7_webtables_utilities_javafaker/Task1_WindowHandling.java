4bb5ebad71be4999bf0e86a9707ab1d6a4f572bc
metkaergul <metkaergul@gmail.com>
2022-09-04 12:38:47 +0300

    browser utils method

--- src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/Task1_WindowHandling.java
+++ src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/Task1_WindowHandling.java
@@ -23,7 +23,7 @@ public class Task1_WindowHandling {
         driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        //3. Go to: https://www.amazon.com
+        //2. Go to: https://www.amazon.com
         driver.get("https://www.amazon.com");
 
     }
@@ -31,12 +31,12 @@ public class Task1_WindowHandling {
 
     @Test
     public void window_handling_test() {
-
+       // 3. Copy paste the lines from below into your class
         ((JavascriptExecutor) driver).executeScript("window.open('https://google.com','_blank');");
         ((JavascriptExecutor) driver).executeScript("window.open('https://etsy.com','_blank');");
         ((JavascriptExecutor) driver).executeScript("window.open('https://facebook.com','_blank');");
 
-
+        //4. Create a logic to switch to the tab where Etsy.com is open
         Set<String> allWindowHandles = driver.getWindowHandles();
         for (String eachWindow : allWindowHandles) {
 
@@ -48,6 +48,7 @@ public class Task1_WindowHandling {
 
 
         }
+        //5. Assert: Title contains “Etsy”
         String actualCurrentTitle= driver.getTitle();
         String expectedInTitle= "etsy";
         Assert.assertTrue(actualCurrentTitle.contains(expectedInTitle));
---
+++ src/test/java/com/cydeo/utilities/BrowserUtils.java
@@ -0,0 +1,20 @@
+package com.cydeo.utilities;
+
+public class BrowserUtils {
+
+/*this method accepts int(in seconds) and execute Thread.sleep for given duration
+
+ */
+
+    public void sleep (int second){
+        second*=1000;
+
+        try {
+            Thread.sleep(second);
+        } catch(InterruptedException e){
+
+            }
+
+    }
+
+}
